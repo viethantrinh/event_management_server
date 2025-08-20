be76bb01216a1d93904a84bbce77e3c0d83e2e89
viethantrinh <hntrnn12@gmail.com>
2025-08-20 13:15:03 +0800

    switch active profile from dev to prod

--- src/main/java/com/trvihnls/bootstraps/AppInitialization.java
+++ src/main/java/com/trvihnls/bootstraps/AppInitialization.java
@@ -5,8 +5,6 @@ import com.trvihnls.domains.User;
 import com.trvihnls.enums.ErrorCodeEnum;
 import com.trvihnls.enums.RoleEnum;
 import com.trvihnls.exceptions.AppException;
-import com.trvihnls.repositories.DutyRepository;
-import com.trvihnls.repositories.EventRepository;
 import com.trvihnls.repositories.RoleRepository;
 import com.trvihnls.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -22,8 +20,6 @@ public class AppInitialization implements CommandLineRunner {
 
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
-    private final DutyRepository dutyRepository;
-    private final EventRepository eventRepository;
     private final PasswordEncoder passwordEncoder;
 
     @Override
@@ -32,21 +28,23 @@ public class AppInitialization implements CommandLineRunner {
     }
 
     private void initializeSampleUser() {
-        Role adminRole = roleRepository.findByName(RoleEnum.ADMIN.getName())
-                .orElseThrow(() -> new AppException(ErrorCodeEnum.REGISTERED_FAILED_ROLE_NOT_EXISTED));
+        if (!userRepository.existsByEmail("abc")) {
+            Role adminRole = roleRepository.findByName(RoleEnum.ADMIN.getName())
+                    .orElseThrow(() -> new AppException(ErrorCodeEnum.REGISTERED_FAILED_ROLE_NOT_EXISTED));
 
-        User user0 = User.builder()
-                .id("190ce620-7375-4d96-b2da-41bc21a19507")
-                .email("abc")
-                .password(passwordEncoder.encode("123456"))
-                .fullName("abc")
-                .phoneNumber("0768701056")
-                .academicRank("GVC")
-                .academicDegree("TS")
-                .roles(Set.of(adminRole))
-                .build();
+            User user0 = User.builder()
+                    .id("190ce620-7375-4d96-b2da-41bc21a19507")
+                    .email("abc")
+                    .password(passwordEncoder.encode("123456"))
+                    .fullName("abc")
+                    .phoneNumber("0768701056")
+                    .academicRank("GVC")
+                    .academicDegree("TS")
+                    .roles(Set.of(adminRole))
+                    .build();
 
-        userRepository.save(user0);
+            userRepository.save(user0);
+        }
     }
 
 
